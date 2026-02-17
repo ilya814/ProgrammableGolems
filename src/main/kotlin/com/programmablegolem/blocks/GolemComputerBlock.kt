2ea6fe2d9023a66a53ea6c65f4f67016f14bf9ca
ilya814 <ilyatascken@gmail.com>
2026-02-17 14:09:25 +0100

    Update GolemComputerBlock.kt

--- src/main/kotlin/com/programmablegolem/blocks/GolemComputerBlock.kt
+++ src/main/kotlin/com/programmablegolem/blocks/GolemComputerBlock.kt
@@ -2,86 +2,56 @@ package com.programmablegolem.blocks
 
 import com.programmablegolem.blockentity.GolemComputerBlockEntity
 import com.programmablegolem.blockentity.ModBlockEntities
+import com.programmablegolem.entity.GolemComponent
+import com.programmablegolem.network.OpenScreenPayload
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.InteractionResult
+import net.minecraft.world.entity.animal.IronGolem
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.EntityBlock
-import net.minecraft.world.level.block.SoundType
+import net.minecraft.world.level.block.BaseEntityBlock
+import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.material.MapColor
+import net.minecraft.world.phys.AABB
 import net.minecraft.world.phys.BlockHitResult
 
-class GolemComputerBlock : Block(
-    Properties.of()
-        .mapColor(MapColor.METAL)
-        .strength(3.5f)
-        .sound(SoundType.METAL)
-        .requiresCorrectToolForDrops()
-), EntityBlock {
-    
-    override fun createBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
-        return GolemComputerBlockEntity(pos, state)
+class GolemComputerBlock : BaseEntityBlock(BlockBehaviour.Properties.of().strength(3.5f).requiresCorrectToolForDrops()) {
+
+    override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity =
+        GolemComputerBlockEntity(pos, state)
+
+    override fun <T : BlockEntity> getTicker(level: Level, state: BlockState, type: BlockEntityType<T>): BlockEntityTicker<T>? {
+        return if (level.isClientSide) null
+        else createTickerHelper(type, ModBlockEntities.GOLEM_COMPUTER, GolemComputerBlockEntity::tick)
     }
-    
-    override fun <T : BlockEntity?> getTicker(
-        level: Level,
-        state: BlockState,
-        type: BlockEntityType<T>
-    ): BlockEntityTicker<T>? {
-        return if (level.isClientSide) {
-            null
-        } else {
-            createTickerHelper(type, ModBlockEntities.GOLEM_COMPUTER) { world, pos, blockState, blockEntity ->
-                blockEntity.tick(world, pos, blockState)
+
+    override fun getRenderShape(state: BlockState) = RenderShape.MODEL
+
+    override fun useWithoutItem(state: BlockState, level: Level, pos: BlockPos, player: Player, hitResult: BlockHitResult): InteractionResult {
+        if (level.isClientSide) return InteractionResult.SUCCESS
+        val be = level.getBlockEntity(pos) as? GolemComputerBlockEntity ?: return InteractionResult.FAIL
+
+        if (be.connectedGolemUUID == null) {
+            val box = AABB(pos).inflate(10.0)
+            val golem = level.getEntitiesOfClass(IronGolem::class.java, box).firstOrNull { golem ->
+                val comp = GolemComponent.get(golem)
+                comp.isCabled && comp.connectedPlayerUUID == player.uuid
+            }
+            if (golem != null) {
+                be.connectedGolemUUID = golem.uuid
+                be.setChanged()
             }
         }
-    }
-    
-    @Suppress("DEPRECATION")
-    override fun useWithoutItem(
-        state: BlockState,
-        level: Level,
-        pos: BlockPos,
-        player: Player,
-        hitResult: BlockHitResult
-    ): InteractionResult {
-        if (!level.isClientSide) {
-            val blockEntity = level.getBlockEntity(pos)
-            if (blockEntity is GolemComputerBlockEntity && player is ServerPlayer) {
-                // Send packet to open GUI
-                val buf = io.netty.buffer.Unpooled.buffer()
-                val packetBuf = net.minecraft.network.FriendlyByteBuf(buf)
-                packetBuf.writeBlockPos(pos)
-                packetBuf.writeBoolean(blockEntity.connectedGolemUUID != null)
-                packetBuf.writeBoolean(blockEntity.isDownloading)
-                packetBuf.writeInt(blockEntity.downloadProgress)
-                
-                net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking.send(
-                    player,
-                    com.programmablegolem.network.ModNetworking.OPEN_COMPUTER_SCREEN_PACKET,
-                    packetBuf
-                )
-            }
+
+        if (player is ServerPlayer) {
+            ServerPlayNetworking.send(player, OpenScreenPayload(pos, be.connectedGolemUUID != null, be.isDownloading, be.downloadProgress))
         }
         return InteractionResult.SUCCESS
-    }
-    
-    companion object {
-        fun <E : BlockEntity, A : BlockEntity> createTickerHelper(
-            givenType: BlockEntityType<A>,
-            expectedType: BlockEntityType<E>,
-            ticker: BlockEntityTicker<in E>
-        ): BlockEntityTicker<A>? {
-            return if (expectedType == givenType) {
-                @Suppress("UNCHECKED_CAST")
-                ticker as BlockEntityTicker<A>
-            } else null
-        }
     }
 }
