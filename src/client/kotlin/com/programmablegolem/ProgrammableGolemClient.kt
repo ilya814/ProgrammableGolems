4f6f1209a3d0818c31b136d79988024b9ba85659
ilya814 <ilyatascken@gmail.com>
2026-02-17 14:31:34 +0100

    Update ProgrammableGolemClient.kt

--- src/client/kotlin/com/programmablegolem/ProgrammableGolemClient.kt
+++ src/client/kotlin/com/programmablegolem/ProgrammableGolemClient.kt
@@ -1,10 +1,18 @@
 package com.programmablegolem
 
-import com.programmablegolem.network.ModNetworking
+import com.programmablegolem.gui.GolemComputerScreen
+import com.programmablegolem.network.OpenScreenPayload
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 
 object ProgrammableGolemClient : ClientModInitializer {
     override fun onInitializeClient() {
-        ModNetworking.registerClientPackets()
+        ClientPlayNetworking.registerGlobalReceiver(OpenScreenPayload.TYPE) { payload, context ->
+            context.client().execute {
+                context.client().setScreen(
+                    GolemComputerScreen(payload.computerPos, payload.isConnected, payload.isDownloading, payload.progress)
+                )
+            }
+        }
     }
 }
